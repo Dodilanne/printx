ed3553585916f35236f5d09bc66c5bd768ec75f0
Dodilanne <dodilanne@ada-anvil.io>
2025-08-29 14:02:31 -0400

    Handle different input and output formats

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-import { Transaction } from "@emurgo/cardano-serialization-lib-nodejs-gc";
+import * as CSL from "@emurgo/cardano-serialization-lib-nodejs-gc";
 
 async function getInput(): Promise<string | undefined> {
   try {
-    const arg = Deno.args[0];
-    if (arg?.length) {
-      return arg;
+    if (Deno.args.length > 0 && !Deno.args[0].startsWith("-")) {
+      return Deno.args[0];
     }
 
     if (Deno.stdin.isTerminal()) {
@@ -22,23 +21,83 @@ async function getInput(): Promise<string | undefined> {
     }
 
     const piped = input.trim();
-    return piped.length > 0 ? piped : undefined;
+    if (piped.length > 0) {
+      return piped;
+    }
+
+    return undefined;
   } catch (error) {
     console.error(error);
     Deno.exit(1);
   }
 }
 
+const argDefs = [
+  { short: "c", long: "constructor" },
+  { short: "i", long: "input" },
+  { short: "o", long: "output" },
+] as const;
+
+type Args = Record<typeof argDefs[number]["long"], string>;
+
+const defaultArgs: Args = {
+  constructor: "Transaction",
+  input: "hex",
+  output: "json",
+};
+
+function getArgs(): Args {
+  const args: Partial<Record<typeof argDefs[number]["long"], string>> = {};
+  for (let i = 0; i < Deno.args.length; i++) {
+    const arg = Deno.args[i];
+
+    let argDef: typeof argDefs[number] | undefined = undefined;
+    if (arg.startsWith("--")) {
+      argDef = argDefs.find((a) => a.long === arg.slice(2));
+    } else if (arg.startsWith("-")) {
+      argDef = argDefs.find((a) => a.short === arg.slice(1, 2));
+    }
+
+    if (argDef && i + 1 < Deno.args.length) {
+      args[argDef.long] = Deno.args[i + 1];
+      i++;
+    }
+  }
+  return { ...defaultArgs, ...args };
+}
+
+type AnyConstructor = {
+  [F in `from_${string}`]: (input: string) => {
+    [T in `to_${string}`]: () => unknown;
+  };
+};
+
 export async function main() {
-  const cbor = await getInput();
-  if (!cbor) {
+  const input = await getInput();
+  if (!input) {
     console.error("No CBOR data provided");
     Deno.exit(1);
   }
-  const tx = Transaction.from_hex(cbor);
-  console.log(tx.to_json());
+
+  const cbor = input.replace(/"/g, "");
+
+  const args = getArgs();
+
+  const constructorName = args.constructor as keyof typeof CSL;
+  const constructor = CSL[constructorName] as AnyConstructor;
+  if (!constructor) {
+    throw new Error(`Constructor ${constructorName} not found`);
+  }
+
+  const obj = constructor[`from_${args.input}`](cbor);
+
+  const output = obj[`to_${args.output}`]();
+
+  return output;
 }
 
 if (import.meta.main) {
-  await main();
+  const res = await main();
+  console.log(res);
+  Deno.exit(0);
 }
